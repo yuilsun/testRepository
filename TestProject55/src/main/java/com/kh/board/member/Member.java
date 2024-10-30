0a44624d7ee0131db28460ae625d3a90cbcb7606
break <break@WIN-3VLF6I4E6GR>
2024-10-30 22:52:49 +0900

    자료메시지

--- TestProject55/src/main/java/com/kh/board/member/Member.java
+++ TestProject55/src/main/java/com/kh/board/member/Member.java
@@ -1,39 +1,5 @@
-package com.kh.board.member;
+package src.main.java.com.kh.board.member;
 
 public class Member {
-	
-	String title;
-	int age;
-	String subject;
-	public Member(String title, int age, String subject) {
-		super();
-		this.title = title;
-		this.age = age;
-		this.subject = subject;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public String getSubject() {
-		return subject;
-	}
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-	@Override
-	public String toString() {
-		return "Member [title=" + title + ", age=" + age + ", subject=" + subject + "]";
-	}
-	
-	
 
 }
---
+++ TestProject55/src/main/java/com/kh/board/member/notice/Notice.java
@@ -0,0 +1,7 @@
+package com.kh.board.member.notice;
+
+public class Notice {
+	
+	//공지사항 작성
+
+}
